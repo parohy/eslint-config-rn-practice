53f421228b995cfce0ac382d369f223bc23c84c8
Tomas Paronai <tomas.paronai@globallogic.com>
2018-10-04 10:04:16 +0200

    Initial commit with rules

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules : {
+    "react-native/no-color-literals": 0,
+    "class-methods-use-this": 0,
+    "no-use-before-define": 0,
+    "arrow-parens": ["error", "as-needed"],
+    "prefer-destructuring": ["error", { "VariableDeclarator": { "object": true } }],
+    "no-underscore-dangle": 0
+  }
+}
